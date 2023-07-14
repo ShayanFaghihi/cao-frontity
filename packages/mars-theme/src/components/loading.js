d8bd060fd4dbd654820a3ed9a695d320912242de
Shayan Faghihi <s.faghihi@bettechnology.com>
2023-07-14 09:55:07 +0330

    Styled the loader

--- packages/mars-theme/src/components/loading.js
+++ packages/mars-theme/src/components/loading.js
@@ -1,4 +1,5 @@
 import { styled, keyframes, css } from "frontity";
+import UI from "./UI";
 
 const scale = keyframes`
   0% {transform: scaley(1.0)}
@@ -7,15 +8,15 @@ const scale = keyframes`
 `;
 
 const Loading = () => (
-  <Container>
-    <div>
+  <UI>
+    <div style={{ textAlign: "center" }}>
       <div css={bar(1)} />
       <div css={bar(2)} />
       <div css={bar(3)} />
       <div css={bar(4)} />
       <div css={bar(5)} />
     </div>
-  </Container>
+  </UI>
 );
 
 export default Loading;
@@ -30,17 +31,4 @@ const bar = (index) => css`
   animation: ${scale} 1s ${index * 0.1}s infinite
     cubic-bezier(0.2, 0.68, 0.18, 1.08);
   animation-fill-mode: both;
-`;
-
-const Container = styled.div`
-  width: 800px;
-  margin: 0;
-  padding: 24px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  & > * {
-    margin-top: 24px;
-  }
 `;
