a7bb5c46dabbf67cf23c1b9241399084b749f1df
Shayan Faghihi <s.faghihi@bettechnology.com>
2023-08-28 06:02:24 +0330

    CompareFooter added to SearchResuls page

--- packages/mars-theme/src/components/pages/SearchResults.jsx
+++ packages/mars-theme/src/components/pages/SearchResults.jsx
@@ -1,11 +1,15 @@
-import { connect, styled } from "frontity";
+import { useContext } from "react";
+import { connect } from "frontity";
 import UI from "../UI";
 import AppsList from "../list/AppsList";
 import SearchField from "../SearchField";
+import CompareContext from "../../context/compare-context";
+import CompareFooter from "../CompareFooter";
 
 const reverseFormat = (query) => query.replace("+", " ");
 
 const SearchResults = ({ state }) => {
+  const compareCtx = useContext(CompareContext);
   // Get information about the current URL.
   const data = state.source.get(state.router.link);
   // data.searchQuery → query done to get search results
@@ -20,6 +24,7 @@ const SearchResults = ({ state }) => {
         </div>
         <AppsList query="search" searchQuery={reverseFormat(searchQuery)} />
       </UI>
+      <CompareFooter visible={compareCtx.isTwoAppSelected} />
     </>
   );
 };
